94e5cc49738df20acd1888bca6ac46aace606256
Adarsh Verma <nivsmehta4491@gmail.com>
2021-09-17 17:12:50 +0530

    Add default Plugin files

--- includes/easy-reservations-reviews-functions.php
+++ includes/easy-reservations-reviews-functions.php
@@ -9,33 +9,27 @@ if ( ! function_exists( 'ersrvr_setting_fields' ) ) {
 	 * @return array
 	 * @since 1.0.0
 	 */
-    function ersrvr_setting_fields(){
-        $fields = array(
-            array(
-                'title' => __( 'Reservations Reviews Setting', 'easy-reservations-reviews' ),
-                'type'  => 'title',
-                'desc'  => '',
-                'id'    => 'ersrvr_reviews_settings',
-            ),
-            array(
-                'name' => __( 'Enable', 'easy-reservations-reviews' ),
-                'type' => 'checkbox',
-                'desc' => __( 'This will decide whether the customers can add reviews. Default is no.', 'easy-reservations-reviews' ),
-                'id'   => 'ersrvr_enable_reviews_settings',
-            ),
-            array(
-                'name'        => __( 'Submit Review Button Text', 'easy-reservations-reviews' ),
-                'type'        => 'text',
-                'desc'        => __( 'This holds the submit reviews button text button text. Default: Submit Review', 'easy-reservations-reviews' ),
-                'desc_tip'    => true,
-                'id'          => 'ersrv_submit_review_button_text',
-                'placeholder' => __( 'E.g.: Submit Review', 'easy-reservations-reviews' ),
-            ),
-            array(
-                'type' => 'sectionend',
-                'id'   => 'ersrvr_reviews_settings',
-            ),
-        );
-        return $fields;
-    }
+	function ersrvr_setting_fields(){
+		$fields = array(
+			array(
+				'title' => __( 'Reservations Reviews Setting', 'easy-reservations-reviews' ),
+				'type'  => 'title',
+				'desc'  => '',
+				'id'    => 'ersrvr_reviews_settings',
+			),
+			array(
+				'name'        => __( 'Submit Review Button Text', 'easy-reservations-reviews' ),
+				'type'        => 'text',
+				'desc'        => __( 'This holds the submit reviews button text button text. Default: Submit Review', 'easy-reservations-reviews' ),
+				'desc_tip'    => true,
+				'id'          => 'ersrv_submit_review_button_text',
+				'placeholder' => __( 'E.g.: Submit Review', 'easy-reservations-reviews' ),
+			),
+			array(
+				'type' => 'sectionend',
+				'id'   => 'ersrvr_reviews_settings',
+			),
+		);
+		return $fields;
+	}
 }
