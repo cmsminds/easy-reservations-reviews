9188d11a693c9440d4dabd27d78c8093c00e91c5
MubinMansuri <mubinmansuri41@gmail.com>
2021-09-21 18:46:22 +0530

    add from html...

--- public/templates/woocommerce/easy-reservations-reviews-html.php
+++ public/templates/woocommerce/easy-reservations-reviews-html.php
@@ -14,12 +14,42 @@
 ?>
 
 <div class="ship-reviews info-box">
-    <a class="section-title font-Poppins font-size-24 font-weight-bold d-block color-black text-decoration-none" data-toggle="collapse" href="#ship-reviews-collapse" role="button" aria-expanded="false" aria-controls="ship-reviews-collapse">
+    <a class="section-title font-Poppins font-size-24 font-weight-bold d-block color-black text-decoration-none" data-toggle="collapse" href="#ship-reviews-collapse" role="button" aria-expanded="true" aria-controls="ship-reviews-collapse">
         <span class="">Reviews</span>
     </a>
-    <div class="collapse" id="ship-reviews-collapse">
+    <div class="collapse show" id="ship-reviews-collapse">
         <div class="dropdown-divider"></div>
-        <p>show list of reviews here...</p>
+
+        <div class="alert alert-warning" role="alert">
+            You are logged in as <span class="font-weight-bold">hiren@magicontap.com</span>. If you would like to use a different account for this mmbership, <a href="#" class="">Log out now</a>.
+        </div>
+
+        <!-- form start here -->
+        <!-- name, email, phone, file upload button, review message textarea ye fields chaiye hongi hume -->
+        <div class="review-form-wrapper">
+            <form action="#" method="post" enctype="multipart/form-data">
+                <div class="form-row">
+                    <div class="col-12">
+                        <label class="sr-only" for="name">Name</label>
+                        <input type="text" class="form-control mb-2" id="name" placeholder="Name" />
+                    </div>
+                    <div class="col-12">
+                        <label class="sr-only" for="email">Email</label>
+                        <input type="email" class="form-control mb-2" id="email" placeholder="E-mail" />
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-12">
+                        <label class="sr-only" for="phone">Phone Number</label>
+                        <input type="text" class="form-control mb-2" id="phone" placeholder="Phone Number" />
+                    </div>
+                    <div class="col-12">
+                        <label class="sr-only" for="message">Message</label>
+                        <textarea name="message" id="message" class="form-control mb-2"  placeholder="E-mail"></textarea>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
