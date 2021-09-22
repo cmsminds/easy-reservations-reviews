bcb4ea9a7fe6ba281e45e10616b856054dde8e38
Adarsh Verma <nivsmehta4491@gmail.com>
2021-09-22 18:36:06 +0530

    PHPCS issue fixing

--- public/class-easy-reservations-reviews-public.php
+++ public/class-easy-reservations-reviews-public.php
@@ -107,7 +107,59 @@ class Easy_Reservations_Reviews_Public {
 		}
 
 		ob_start();
-		echo wp_kses( ersrvr_prepare_reviews_html() );
+		echo wp_kses(
+			ersrvr_prepare_reviews_html(),
+			array(
+				'div'      => array(
+					'class' => array(),
+					'id'    => array(),
+					'role'  => array(),
+				),
+				'span'     => array(
+					'class' => array(),
+				),
+				'p'        => array(),
+				'a'        => array(
+					'href'          => array(),
+					'class'         => array(),
+					'download'      => array(),
+					'data-toggle'   => array(),
+					'role'          => array(),
+					'aria-expanded' => array(),
+					'aria-controls' => array(),
+				),
+				'h1'       => array(),
+				'button'   => array(
+					'type'  => array(),
+					'class' => array(),
+				),
+				'input'    => array(
+					'type'        => array(),
+					'name'        => array(),
+					'id'          => array(),
+					'accept'      => array(),
+					'placeholder' => array(),
+					'class'       => array(),
+					'value'       => array(),
+					'hidden'      => array(),
+				),
+				'form'     => array(
+					'method'  => array(),
+					'enctype' => array(),
+					'action'  => array(),
+				),
+				'label'    => array(
+					'class' => array(),
+					'for'   => array(),
+				),
+				'textarea' => array(
+					'name'        => array(),
+					'id'          => array(),
+					'class'       => array(),
+					'placeholder' => array(),
+				),
+			),
+		);
 		return ob_get_clean();
 	}
 	/**
