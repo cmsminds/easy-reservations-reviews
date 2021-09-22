8c0e6068ddcaa6eb66441e5cae227e964cb11004
MubinMansuri <mubinmansuri41@gmail.com>
2021-09-22 18:20:26 +0530

    Form HTML /CSS added ...

--- includes/easy-reservations-reviews-functions.php
+++ includes/easy-reservations-reviews-functions.php
@@ -233,21 +233,42 @@ if ( ! function_exists( 'ersrvr_prepare_reviews_form_html' ) ) {
 		<form action="#" method="post" enctype="multipart/form-data">
 			<div class="form-row">
 				<div class="col-12">
-					<label for="">Please reate us 1 (bad) to 5 (excellent) *</label>
-					<div id="full-stars-example-two" class="rating-group-wrapper border">
-						<div class="rating-group">
-							<input disabled checked class="rating__input rating__input--none" name="rating3" id="rating3-none" value="0" type="radio">
-							<label aria-label="1 star" class="rating__label" for="rating3-1"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
-							<input class="rating__input" name="rating3" id="rating3-1" value="1" type="radio">
-							<label aria-label="2 stars" class="rating__label" for="rating3-2"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
-							<input class="rating__input" name="rating3" id="rating3-2" value="2" type="radio">
-							<label aria-label="3 stars" class="rating__label" for="rating3-3"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
-							<input class="rating__input" name="rating3" id="rating3-3" value="3" type="radio">
-							<label aria-label="4 stars" class="rating__label" for="rating3-4"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
-							<input class="rating__input" name="rating3" id="rating3-4" value="4" type="radio">
-							<label aria-label="5 stars" class="rating__label" for="rating3-5"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
-							<input class="rating__input" name="rating3" id="rating3-5" value="5" type="radio">
+					<label class="font-Poppins font-weight-semibold text-black font-size-14">Please reate us 1 (bad) to 5 (excellent) <span class="text-danger">*</span></label>
+					<div id="full-stars-example-two" class="rating-group-wrapper border py-2 px-1 rounded-xl">
+						<!-- rating items starts here -->
+						<div class="rating-item d-flex flex-wrap align-items-center">
+							<div class="col-4 col-sm-3"><label class="font-Poppins font-weight-semibold text-black font-size-14"><?php esc_html_e( 'Features', 'easy-reservations-reviews' ); ?> </label></div>
+							<div class="col-8 col-sm-9 rating-group">
+								<input disabled checked class="rating__input rating__input--none" name="rating3" id="rating3-none" value="0" type="radio">
+								<label aria-label="1 star" class="rating__label" for="rating3-1"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-1" value="1" type="radio">
+								<label aria-label="2 stars" class="rating__label" for="rating3-2"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-2" value="2" type="radio">
+								<label aria-label="3 stars" class="rating__label" for="rating3-3"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-3" value="3" type="radio">
+								<label aria-label="4 stars" class="rating__label" for="rating3-4"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-4" value="4" type="radio">
+								<label aria-label="5 stars" class="rating__label" for="rating3-5"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-5" value="5" type="radio">
+							</div>
 						</div>
+						<div class="rating-item d-flex flex-wrap align-items-center">
+							<div class="col-4 col-sm-3"><label class="font-Poppins font-weight-semibold text-black font-size-14"><?php esc_html_e( 'Features', 'easy-reservations-reviews' ); ?> </label></div>
+							<div class="col-8 col-sm-9 rating-group">
+								<input disabled checked class="rating__input rating__input--none" name="rating3" id="rating3-none" value="0" type="radio">
+								<label aria-label="1 star" class="rating__label" for="rating3-1"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-1" value="1" type="radio">
+								<label aria-label="2 stars" class="rating__label" for="rating3-2"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-2" value="2" type="radio">
+								<label aria-label="3 stars" class="rating__label" for="rating3-3"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-3" value="3" type="radio">
+								<label aria-label="4 stars" class="rating__label" for="rating3-4"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-4" value="4" type="radio">
+								<label aria-label="5 stars" class="rating__label" for="rating3-5"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+								<input class="rating__input" name="rating3" id="rating3-5" value="5" type="radio">
+							</div>
+						</div>
+
 					</div>
 				</div>
 			</div>
