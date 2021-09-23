c65be72949f552584875b4dfe856cfcce15da23d
Adarsh Verma <nivsmehta4491@gmail.com>
2021-09-23 12:10:36 +0530

    Changes

--- includes/easy-reservations-reviews-functions.php
+++ includes/easy-reservations-reviews-functions.php
@@ -258,8 +258,8 @@ if ( ! function_exists( 'ersrvr_prepare_reviews_form_html' ) ) {
 									<div class="col-4 col-sm-3"><label class="font-Poppins font-weight-semibold text-black font-size-14"><?php echo esc_html( $criteria ); ?> </label></div>
 									<div class="col-8 col-sm-9 rating-group">
 										<?php for ( $i = 1; $i <= 5; $i++ ) { ?>
-											<label aria-label="<?php echo esc_attr( $i ); ?> star" class="rating__label" for="rating3-<?php echo esc_attr( $i ); ?>"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
-											<input class="rating__input" name="rating3" id="rating3-<?php echo esc_attr( $i ); ?>" value="<?php echo esc_attr( $i ); ?>" type="radio">
+											<label aria-label="<?php echo esc_attr( $i ); ?> star" class="rating__label" for="rating<?php echo esc_attr( $i ); ?>-<?php echo esc_attr( $i ); ?>"><span class="rating__icon rating__icon--star fa fa-star"></span></label>
+											<input class="rating__input" name="rating3" id="rating<?php echo esc_attr( $i ); ?>-<?php echo esc_attr( $i ); ?>" value="<?php echo esc_attr( $i ); ?>" type="radio">
 										<?php } ?>
 									</div>
 								</div>
