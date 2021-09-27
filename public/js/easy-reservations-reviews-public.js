b8d2bdce30e7b4fe1ad04fbc90cfcde640d9b3a2
Adarsh Verma <adarsh.srmcem@gmail.com>
2021-09-27 20:09:56 +0530

    multiple similar criteria rating issue fixed

--- public/js/easy-reservations-reviews-public.js
+++ public/js/easy-reservations-reviews-public.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready( function( $ ) {
 	'use strict';
 
-	var favorite = [];
+	var user_criteria_ratings = [];
 	jQuery(document).on( 'mouseout', '.rating__label', function( evt ) {
 		// evt.preventDefault();
 		var this_label        = $( this );
@@ -23,8 +23,11 @@ jQuery( document ).ready( function( $ ) {
 		$( '#' + criteria_input_id ).prevUntil( '.rating__input:first' ).addBack().addClass( 'fill_star_hover' );
 		$( 'label.rating__label' ).removeClass( 'fill_star_hover' );
 	} );
-	
-	jQuery(document).on( 'click', '.rating__label', function( evt ) {
+
+	/**
+	 * Add the star rating.
+	 */
+	jQuery( document ).on( 'click', '.rating__label', function() {
 		// evt.preventDefault();
 		$( 'label.rating__label' ).removeClass( 'fill_star_click' );
 		var this_label        = $( this );
@@ -33,18 +36,33 @@ jQuery( document ).ready( function( $ ) {
 		var closest_criteria  = $( this ).closest( '.rating-group' ).attr( 'id' );
 		$( '#' + closest_criteria + ' .rating__input' ).removeClass( 'fill_star_click' );
 		$( '#' + criteria_input_id ).prevUntil( '.rating__input:first' ).addBack().addClass( 'fill_star_click' );
-		var ratings = criteria_input.val();
+		var rating = parseInt( criteria_input.val() );
 		$( 'label.rating__label' ).removeClass( 'fill_star_click' );
-		console.log( ratings );
-		favorite.push( {
-			closest_criteria: closest_criteria, 
-			criteria_review_value: ratings,
+
+		// Check if criteria to add already exists in the array.
+		var existing_criteria_key = $.map( user_criteria_ratings, function( val, i ) {
+			if ( val.closest_criteria === closest_criteria ) {
+				return i;
+			}
 		} );
-	});
-	console.log(favorite);
+
+		// If the key is found.
+		if ( 0 < existing_criteria_key.length ) {
+			user_criteria_ratings.splice( existing_criteria_key[0], 1 );
+		}
+
+		// Push the element in the array.
+		user_criteria_ratings.push( {
+			closest_criteria: closest_criteria,
+			rating: rating,
+		} );
+
+		console.log( 'user_criteria_ratings', user_criteria_ratings );
+	} );
+
 	jQuery(document).on( 'click', '.ersrvr_btn_submit', function( evt ) {
 		evt.preventDefault();
-		console.log(favorite);
+		console.log(user_criteria_ratings);
 	} );
 	/**
 	 * Check if a string is valid.
